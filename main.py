19071e4c43f8f71947d8c615053039eb82074784
om patel <omsuperson@gmail.com>
2023-06-27 11:49:17 +0530

    Final version has been made.

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
-from tkinter import *
+import tkinter as tk
 import time
 import threading
 import random
 
+
 class FlashFingersGUI:
-
-
-    def __int__(self):
-        self.root = Tk()
+    def __init__(self):
+        self.root = tk.Tk()
         self.root.title("FlashFingers")
         self.root.geometry("800x600")
 
         self.texts = open("sample Texts.txt", "r").read().split("\n")
 
-        self.frame = Frame(self.root)
+        self.frame = tk.Frame(self.root)
 
-        self.sample_label = Label(self.frame, text=random.choice(self.texts),font=("Helvetica", 15))
+        self.sample_label = tk.Label(self.frame, text=random.choice(self.texts),font=("Helvetica", 15))
         self.sample_label.grid(row=0,column=0,columnspan=2,padx=5,pady=10)
 
-        self.input_entry = Entry(self.frame, width=40, font=("Helvetica",18))
+        self.input_entry = tk.Entry(self.frame, width=40, font=("Helvetica",18))
         self.input_entry.grid(row=1,column=0,columnspan=2,padx=5,pady=10)
-        self.input_entry.bind("<KeyDown>", self.start)
+        self.input_entry.bind("<KeyPress>", self.start)
 
-        self.speed_label = Label(self.frame, text="Speed:\n 0.00 WPM", font=("Helvetica", 15))
+        self.speed_label = tk.Label(self.frame, text="Speed:\n 0.00 WPM", font=("Helvetica", 15))
         self.speed_label.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
 
-        self.reset_button = Button(self.frame, text="Reset", command=self.reset)
+        self.reset_button = tk.Button(self.frame, text="Reset", command=self.reset)
         self.reset_button.grid(row=4, column=0, columnspan=2, padx=5, pady=10)
 
         self.frame.pack(expand=True)
 
         self.counter = 0
-        self.running = True
+        self.running = False
 
         self.root.mainloop()
 
+    def start(self, event):
+        if not self.running:
+            if event.keycode not in [16, 17, 18]:
+                self.running = True
+                t = threading.Thread(target=self.time_thread)
+                t.start()
+        if not self.sample_label.cget("text").startswith(self.input_entry.get()):
+            self.input_entry.config(fg="red")
+        else:
+            self.input_entry.config(fg="black")
+        if self.input_entry.get() == self.sample_label.cget("text")[:-1]:
+            self.running = False
+            self.input_entry.config(fg="green")
 
+    def time_thread(self):
+        while self.running:
+            time.sleep(0.1)
+            self.counter += 0.1
+            cps = len(self.input_entry.get()) / self.counter
+            cpm = cps * 60
+            wpm = cpm / 5
+            self.speed_label.config(text=f"Speed:\n CPM: {cpm: .2f}\n WPM: {wpm: .2f}")
+
+    def reset(self):
+        self.running = False
+        self.counter = 0
+        self.speed_label.config(text="Speed:\n 0.00 WPM")
+        self.sample_label.config(text=random.choice(self.texts))
+        self.input_entry.delete(0, tk.END)
+
+
+
+FlashFingersGUI()
